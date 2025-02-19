384070f6ba2e205087b9295d950f10e11728774f
Katra <katra@katra.local>
2025-02-19 13:15:07 -0700

    Adding support for arbitrary numbers of services and pods

---
+++ terraform-deployment-v2/vars.tf
@@ -0,0 +1,29 @@
+// building while minikube's docker is enabled leads to some auto-garbled tags by default
+variable "image_name_prefix" {
+  description = "Prefix to an image tag minikube prepends to images by default"
+  type = string
+  default = "docker.io/library/"
+  nullable = true
+}
+
+// I'd rather avoid using "latest" as a tag
+variable "version_tag" {
+  description = "Hexidecimal image version tag based on the git commit"
+	type = string
+	default = "latest"
+}
+
+variable "pullfromremote" {
+  description = "Value for imagePullRemote so it can be set to Never for minikube"
+  type = string
+  default = "Never"
+}
+
+
+
+
+// create: a hyphenated and non-hyphenated service name
+// csv format is service_name, port     
+locals {
+  services = { for sname, port in csvdecode(file(./data/svcinfo.csv)) : sname => port }
+}
